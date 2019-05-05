0130a864b689a695e832087b86fed8f3c731cfd5
CarolinJohnny <carolinjohnny@googlemail.com>
2019-05-05 21:55:50 +0200

    Beautify CSS, index, js

--- js/app.js
+++ js/app.js
@@ -1,51 +1,45 @@
 // Enemies our player must avoid
 var Enemy = function(row, column, speed) {
-  // Variables applied to each of our instances go here,
-  // we've provided one for you to get started
+  // Variables applied to each of our instances go here
   this.x = row;
   this.y = column;
   this.speed = speed;
 
-  // The image/sprite for our enemies, this uses
-  // a helper we've provided to easily load images
+  // The image/sprite for our enemies
   this.sprite = 'images/Star.png';
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-  // You should multiply any movement by the dt parameter
-  // which will ensure the game runs at the same speed for
-  // all computers.
+  // Multiply any movement by the dt parameter
+  // which will ensure the game runs at the same speed for all computers.
   this.x += dt * this.speed;
   if (this.x >= 505) {
     this.x = -60;
     this.speed = 200 + Math.floor(Math.random() * 100 + 1)
   }
-  if(player.x + 50 > this.x && player.x < this.x + 50 && player.y < this.y + 50
-   && player.y +50 > this.y) {
-      player.reset();
-    }
+  if (player.x + 50 > this.x && player.x < this.x + 50 && player.y < this.y + 50 &&
+    player.y + 50 > this.y) {
+    player.reset();
+  }
 };
-
-
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
+// Player class
 var Player = function(row, column) {
   this.x = row;
   this.y = column;
 
-  // The image/sprite for our player, this uses
-  // a helper we've provided to easily load images
+  // The image/sprite for our player
   this.sprite = 'images/char-princess-girl.png';
 };
 
-// This class requires an update() method
+// This class requires an update() method, Position and open Modal
 Player.prototype.update = function() {
   if (this.y > 400) {
     this.y = 400;
@@ -69,7 +63,7 @@ Player.prototype.render = function() {
   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-Player.prototype.reset = function(){
+Player.prototype.reset = function() {
   this.x = 200;
   this.y = 500;
 };
@@ -90,21 +84,14 @@ Player.prototype.handleInput = function(KeyDown) {
       this.y += 90;
       break;
   }
-
-
-
 };
 
-
-// Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 let allEnemies = [new Enemy(0, 75, 230), new Enemy(0, 160, 400), new Enemy(0, 240, 500), new Enemy(0, 75, 230)];
 // Place the player object in a variable called player
 let player = new Player(200, 500);
 
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+// This listens for key presses and sends the keys to Player.handleInput() method.
 document.addEventListener('keyup', function(e) {
   var allowedKeys = {
     37: 'left',
@@ -118,4 +105,3 @@ document.addEventListener('keyup', function(e) {
 
 // Modal Popup
 let modal = document.querySelector(".modal");
-//let againButton = document.querySelector(".again-button");
